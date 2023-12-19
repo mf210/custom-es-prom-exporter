ecd6939a3815819f095f9e753bb35a358051ba44
Mehdi Faresi <mfaresi210@gmail.com>
2023-12-19 14:26:24 +0330

    Add custom-es-exporter

---
+++ custom-es-exporter/playground.py
@@ -0,0 +1,28 @@
+import os
+
+from elasticsearch import Elasticsearch
+
+
+# you can get an API_KEY from Kibana's managment section :)
+elastic_api_key = "TU5iOGdJd0JERzEyZUhBUVFqOU86dTFUcDRQODVRNmFSYkxwdmJSdHhhZw=="
+
+
+# Create the client instance
+eclient = Elasticsearch(
+    "https://localhost:9200",
+    ca_certs="http_ca.crt",
+    api_key=elastic_api_key
+)
+
+# create an index
+
+resp = eclient.index(
+    index="my_index",
+    # id="my_document_id_1",
+    document={
+        "foo": "foo",
+        "bar": "bar",
+    }
+)
+
+print(resp)
